e95baeddb9e96c76b5589689ac8a848c3789a15d
CorneliusBnn <cbonn@sysprofit.com>
2024-02-16 12:59:22 +0100

    TestSquare Progress following

--- src/Tests/TestSquare.java
+++ src/Tests/TestSquare.java
@@ -11,6 +11,7 @@ import static org.lwjgl.glfw.GLFW.*;
 
 public class TestSquare extends Test {
     Entity2D entity;
+    Entity2D entity2;
     Shader shader;
     Texture texture;
 
@@ -22,6 +23,7 @@ public class TestSquare extends Test {
         shader.SetUniform1i("u_Texture", 0);
         ObjModel model = ObjModelParser.parseOBJ("res/models/square.obj");
         entity = new Entity2D(new Vector2f(), model, texture, shader);
+entity2 = new Entity2D(new Vector2f(50, 50), model, texture, shader);
 
     }
 
@@ -29,12 +31,14 @@ public class TestSquare extends Test {
     public void OnUpdate(float dt) {
         super.OnUpdate(dt);
         entity.translate(new Vector2f(entity.getVelocity()).mul(dt));
+        entity2.translate(new Vector2f((entity.getPosition().x-entity2.getPosition().x), entity.getPosition().x-entity2.getPosition().y).mul(dt));
     }
 
     @Override
     public void OnRender() {
         super.OnRender();
         renderer.DrawEntity2D(entity);
+        renderer.DrawEntity2D(entity2);
     }
 
     @Override
@@ -42,15 +46,19 @@ public class TestSquare extends Test {
         super.OnKeyInput(window, key, scancode, action, mods);
         if (key == GLFW_KEY_W && action == GLFW_PRESS) {
             entity.setVelocity(new Vector2f(0, 100));
+            entity2.setVelocity(new Vector2f(0, -100));
         }
         if (key == GLFW_KEY_A && action == GLFW_PRESS) {
             entity.setVelocity(new Vector2f(-100, 0));
+            entity2.setVelocity(new Vector2f(+100, 0));
         }
         if (key == GLFW_KEY_S && action == GLFW_PRESS) {
             entity.setVelocity(new Vector2f(0, -100));
+            entity2.setVelocity(new Vector2f(0, +100));
         }
         if (key == GLFW_KEY_D && action == GLFW_PRESS) {
             entity.setVelocity(new Vector2f(100, 0));
+            entity2.setVelocity(new Vector2f(-100, 0));
         }
     }
 }
